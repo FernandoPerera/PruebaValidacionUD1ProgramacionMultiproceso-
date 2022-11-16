3635ec32db28bdc6b59b138c7ead5256429d535c
Fernando <you@example.com>
2022-11-16 13:13:43 +0100

    Feature(main.py): Realizado la generación de procesos hijos, y los tiempo de espera requeridos

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+from datetime import datetime
+
+from time import sleep
+
+def father() :  
+     
+     contador = 1   
+      
+     while (contador <= 10) :
+        
+         newPid = os.fork()
+
+         if newPid == 0 : 
+             children()
+         else :
+            childrenPid = newPid
+            date = datetime.now()
+            print('\nIniciando el proceso : %d a las %s \n ' % (childrenPid, date.time().strftime("%H:%M:%S")))
+
+            sleep(10) 
+         contador += 1
+def children() :
+    
+    print('\t········ | Iniciando proceso con PID %d | ········\n' % os.getpid())
+    sleep(3)
+    print('\n\t········ | Terminando el proceso con PID %d  | ········' % os.getpid())
+
+    os._exit(0) 
+    
+father()
